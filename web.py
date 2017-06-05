040974e8a0cc77cadfcc20224e91cfcd00400ded
davidalves1 <alves.david@outlook.com>
2017-06-05 11:42:32 -0300

    Consultar ações específicas

--- web.py
+++ web.py
@@ -2,31 +2,61 @@
 
 # Documentaçao do BeautifulSoup: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
+import json
 import requests
 import sys
+import time
 from bs4 import BeautifulSoup
 
-page = requests.get('https://app.tororadar.com.br/analise/PETR4/')
-soup = BeautifulSoup(page.content, 'html.parser')
+papers_list = ['PETR4', 'VALE5', 'OIBR4']
 
-info = soup.find('div', class_='info')
+for search in papers_list:
+	page = requests.get('https://app.tororadar.com.br/analise/%s/' % search)
 
-if info is None:
-	sys.exit('Informações não encontradas')
+	if page.status_code != 200:
+		print('O papel %s não foi encontrado.' % search)
+		time.sleep(2)
+		pass
 
-paper = info.h1.get_text()
-employee = info.h2.get_text()
+	soup = BeautifulSoup(page.content, 'html.parser')
 
-oscilation = soup.find('div', class_='oscilation')
+	info = soup.find('div', class_='info')
 
-if oscilation is None:
-	sys.exit('Valor não encontradas')
+	if info is None:
+		print('Informações não encontradas para %s' % search)
+		pass
 
-values = oscilation.get_text().strip().split()
+	paper = info.h1.get_text()
+	employee = info.h2.get_text()
 
-data = 'Ação: %s | Empresa: %s | Valor: R$ %s | Oscilação: %s' % (paper, employee, values[1], values[2])
-print(data)
+	oscilation = soup.find('div', class_='oscilation')
 
-f = open('log.txt', 'a')
-f.write(data + '\n')
-f.close()
+	if oscilation is None:
+		print('Informações não encontradas para %s' % search)
+		pass
+
+	values = oscilation.get_text().strip().split()
+
+	analise = soup.find('div', class_='analise-description')
+
+	if analise is None:
+		print('Informações não encontradas para %s' % search)
+		pass
+
+	analise = analise.p.get_text()
+
+	data = json.dumps({
+		'acao': paper,
+		'empresa': employee, 
+		'valor': float(values[1]),
+		'oscilacao': float(values[2][:-1])
+		# 'analise': analise.strip()
+	})
+
+	print(data)
+
+	f = open('log.txt', 'a')
+	f.write(data + '\n')
+	f.close()
+
+	time.sleep(3)
